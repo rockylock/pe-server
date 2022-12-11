f94b70344998e0392fb9fc3fed589e3a2284b0be
miguelv <miguelovega@gmail.com>
2022-12-11 14:44:36 -0800

    Add baskerville-poster project

---
+++ projects/baskerville-poster/header.php
@@ -0,0 +1,23 @@
+
+<!doctype html>
+
+<html lang="en">
+	<head>
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<title>Baskerville | Spacing Tests</title>
+		<meta name="description" content="Recreating a Baskerville poster using CSS Grid.">
+		
+		<link rel="stylesheet" href="css/style.css">
+
+	</head>
+
+	<body>
+		<header class="page-section site-header">
+
+		</header>
+		
+		<main> 
+			<!-- Content -->
