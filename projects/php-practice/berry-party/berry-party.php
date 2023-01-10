53d96a4009407f539b554e4d1410d66a426710a4
miguelv <miguelovega@gmail.com>
2023-01-09 20:19:28 -0800

    Add berry-party php practice

---
+++ projects/php-practice/berry-party/berry-party.php
@@ -0,0 +1,29 @@
+
+<?php 
+
+/* 
+Challenge:
+
+We'll have 4 guests, 15 berries per person; 26 berries per package.
+How many berries are in a package? how many packages do we need? 
+What's the price per box? What's the total?
+*/
+
+$guests = 1;
+$host = "Ivy";
+$people = $guests + 1;
+$berries = 26; 
+
+$boxCost = 4.89; 
+$berriesPerBox = 16; 
+
+$totalBerries = $people * $berries;
+$boxQuantity = ceil($totalBerries / $berriesPerBox); // Rounds up to include the right amount of boxes.
+$totalCost = number_format($boxQuantity * $boxCost, 2); // Set result to include two decimal places. 
+
+$message = $guests . " guests will be attending the party. Each guest should have $berries berries. We'll need $totalBerries berries for the party, including the host, $host. There are $berriesPerBox berries in each package, with each package costing $" . $boxCost . ". So, we should purchase " . $boxQuantity . " boxes, for a total of $" . $totalCost . ".";
+
+echo $message;
+
+
+
