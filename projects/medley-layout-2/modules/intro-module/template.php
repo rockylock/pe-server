d04c8b44bd260790163bf9fb8657ebafe8b1e68d
miguelv <miguelovega@gmail.com>
2023-02-15 20:02:51 -0800

    Add “Little teaser” content and styling to intro-module.

--- projects/medley-layout-2/modules/intro-module/template.php
+++ projects/medley-layout-2/modules/intro-module/template.php
@@ -1,5 +1,7 @@
 
 <intro-module>
+	<p class="teaser">Little teaser</p>
+
 	<h2 class="attention-voice">Heading level 2</h2>
 
 	<p class="calm-voice">This is some body text. This is some body text. This is some body text. This is some body text.</p>
