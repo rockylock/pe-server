b2e2346a89c4ff6b36abbf221338549fff68dd1f
miguelv <miguelovega@gmail.com>
2023-02-17 22:25:56 -0800

    Add initial views.json data.

---
+++ templates/modules/views-module/template.php
@@ -0,0 +1,17 @@
+
+
+<picture-module>
+	<?php 
+
+		$json = file_get_contents("data/views.json");
+		$viewsData = json_decode($json, true);
+
+		//formatCode($viewsData);
+	?>
+
+  
+		<picture class="">
+			<img src="" alt="">
+		</picture>
+	
+</picture-module>
